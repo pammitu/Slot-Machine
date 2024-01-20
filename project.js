6c4b70cbb5762b5f98efdcf9936409cb5ca93596
pammitu <pamstankova@gmail.com>
2024-01-20 10:57:04 -0800

    thinking through basic program steps

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money, we need to know how much money the user has to plat with
+// 2. Determine the number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play agin
